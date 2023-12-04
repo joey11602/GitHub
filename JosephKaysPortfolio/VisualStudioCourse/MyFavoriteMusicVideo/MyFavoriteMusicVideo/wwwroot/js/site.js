1f1e4ed093703d53a73344dbcae627af78f525be
joey11602 <joey35475@outlook.com>
2023-12-03 18:28:40 -0600

    add project MyFavoriteMusicVideo

---
+++ JosephKaysPortfolio/VisualStudioCourse/MyFavoriteMusicVideo/MyFavoriteMusicVideo/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var myAnimation = document.getElementById("myAnimation");
+    var containerWidth = 550;
+    var containerHeight = 439;
+    var squareSize = 50;
+
+    // Define the animation sequence
+    var animations = [
+        { x: 0, y: containerHeight - squareSize },
+        { x: containerWidth - squareSize, y: containerHeight - squareSize },
+        { x: containerWidth - squareSize, y: 0 },
+        { x: 0, y: 0 }
+    ];
+
+    // Function to execute the animations
+    function runAnimations() {
+        animations.forEach(function (animation, index) {
+            setTimeout(function () {
+                myAnimation.style.transform = "translate(" + animation.x + "px, " + animation.y + "px)";
+            }, index * 2000); // 2000ms (2 seconds) delay for each animation
+        });
+
+        // Repeat the animations
+        setTimeout(runAnimations, animations.length * 2000);
+    }
+
+    // Start the animations
+    runAnimations();
+});
