9397ec44d10c6dae3fdeda825a0b77f5913b60f8
joey11602 <joey35475@outlook.com>
2023-12-03 19:17:09 -0600

    cleaned up coments

--- JosephKaysPortfolio/VisualStudioCourse/MyFavoriteMusicVideo/MyFavoriteMusicVideo/wwwroot/js/site.js
+++ JosephKaysPortfolio/VisualStudioCourse/MyFavoriteMusicVideo/MyFavoriteMusicVideo/wwwroot/js/site.js
@@ -1,10 +1,12 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
+﻿// This function waits until all content is loaded before firing
+document.addEventListener("DOMContentLoaded", function () {
+    // This gets the element to be used and assigns the sizes to use
     var myAnimation = document.getElementById("myAnimation");
     var containerWidth = 550;
     var containerHeight = 439;
     var squareSize = 50;
 
-    // Define the animation sequence
+    // This defines the animation sequence by x y coordinates
     var animations = [
         { x: 0, y: containerHeight - squareSize },
         { x: containerWidth - squareSize, y: containerHeight - squareSize },
@@ -12,18 +14,20 @@
         { x: 0, y: 0 }
     ];
 
-    // Function to execute the animations
+    // This function executes the animation sequence by using 'setTimeout' to delay each animation
+    //by 'index'/2000 milliseconds, where 'index'is the index of animation in the 'animations' array
     function runAnimations() {
+        // This function is a loop that iterates over each animation in the 'animations' array
         animations.forEach(function (animation, index) {
             setTimeout(function () {
                 myAnimation.style.transform = "translate(" + animation.x + "px, " + animation.y + "px)";
-            }, index * 2000); // 2000ms (2 seconds) delay for each animation
+            }, index * 2000);
         });
 
-        // Repeat the animations
+        // This function restarts the animation sequence in a loop
         setTimeout(runAnimations, animations.length * 2000);
     }
 
-    // Start the animations
+    // This line calls the runAnimation function to start
     runAnimations();
 });
